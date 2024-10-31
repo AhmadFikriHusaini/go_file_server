c1a3c1475123da5d295c00ecf7e4ba6b4e3a9770
Kaijuu <Ahmadfikri.20030@mhs.unesa.ac.id>
2024-10-31 17:40:43 +0700

    golang fileserver

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
-package go_file_server
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./resources"))
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/", fileServer)
+
+	server := http.Server{
+		Addr:    "localhost:80",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Server is running")
+
+}
